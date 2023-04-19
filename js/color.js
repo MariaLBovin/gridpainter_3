85965885e6c4b6cf27a0a84b2c4ebdbce79756d3
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-04-19 21:32:31 +0200

    fixed the problem with the timer

---
+++ js/color.js
@@ -0,0 +1,8 @@
+let colors = ["red", "gren", "blue", "yellow"];
+let player = [];
+
+function randomizeColor(){
+    const assign = Math.floor(Math.random() * colors.length);
+    return {color:colors[i]};
+}
+
