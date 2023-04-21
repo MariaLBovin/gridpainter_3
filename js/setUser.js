27a5bf0ba3db131d154916c2183c4f3eda16b8db
nanidam <nani.dam@medieinstitutet.se>
2023-04-21 12:15:23 +0200

    test get username

--- js/setUser.js
+++ js/setUser.js
@@ -1,14 +1,28 @@
 import createDesktopTwo from "./createDesktopTwo.js";
-const users = [];
+// const users = [];
 
-export default function setUser (id, username, color) {
-    const user ={id, username, color};
+// export default function setUser (id, username, color) {
+//     const user ={id, username, color};
 
-    users.push(user);
+//     users.push(user);
+
+//     createDesktopTwo();
+    
+//     return user;
+
+    
+// }
+
+import io from '../node_modules/socket.io-client/dist/socket.io.esm.min.js';
+const socket = io('http://localhost:3000');
+
+export default function setUser () {
+    const inputField = document.getElementById('inputName');
+    
+    const playerName = inputField.value;
+    console.log(playerName);
+    socket.emit('login', playerName);
 
     createDesktopTwo();
-    
-    return user;
 
-    
 }
