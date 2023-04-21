bed938378e48dfa17e8b37204b2cdc3226f22c56
tittieth <114925706+tittieth@users.noreply.github.com>
2023-04-21 14:17:48 +0200

    socket.io test

--- js/getPlayer.js
+++ js/getPlayer.js
@@ -1,10 +1,18 @@
 import createDesktopThree from "./createDesktopThree.js";
 import {displayName, colourAttached} from "./createDesktopThree.js";
+import { socket } from "./main.js";
+
+
+const users = [];
 
 export default function getPlayer(){ 
-    const inputName = document.getElementById("inputName");
-    displayName.innerHTML = "Välkommen " + inputName;
-    console.log(displayName, colourAttached);
+    // const inputName = document.getElementById("inputName");
+    // displayName.innerHTML = "Välkommen " + inputName;
+    // console.log(displayName, colourAttached);
     //headerDiv.append(displayName,colourAttached);
     //regPlayer();
+
+    socket.on('getUser', (data) => {
+        console.log(data);
+    })
 }
--- js/main.js
+++ js/main.js
@@ -6,18 +6,32 @@ import createDesktopTwo from "./createDesktopTwo.js";
 
 //import io from '../node_modules/socket.io-client/dist/socket.io.esm.min.js';
 
-const socket = io('http://localhost:3000');
+const userName = document.getElementById('inputName')
+
+export const socket = io('http://localhost:3000');
+
 
 socket.on('connect', () => {
-    console.log('Connected to Socket.IO server');
+    console.log(`Connected to Socket.IO server with id: ${socket.id}`);
 });
+
+socket.emit('userEnter', 1, "hej");
+
+socket.on('getUser', (data) => {
+    console.log(data);
+})
+
   
+
 
 
 export const loginBtn = document.getElementById("loginBtn");
 
 
-loginBtn.addEventListener("click", createDesktopTwo);
+loginBtn.addEventListener("click", ()  => {
+    socket.emit('getUser', userName.value);
+    createDesktopTwo();
+});
 
 
 
