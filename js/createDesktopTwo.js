d7280576fb3f5470f2b12071a701af21653a1f58
MariaLBovin <maria.bovin@gmail.com>
2023-04-22 19:41:47 +0200

    number of users counted

--- js/createDesktopTwo.js
+++ js/createDesktopTwo.js
@@ -1,32 +1,115 @@
 import createDesktopThree from "./createDesktopThree.js";
 import { socket } from "./main.js";
-const users = [];
 
 export default function createDesktopTwo(){
-    socket.on('getUser', (data) => {
-        const user = {userName: data, color: "red"}
-        users.push(user);
-        console.log(users);
-    });
-
     const contentContainer = document.querySelector(".contentContainer");
-    contentContainer.innerHTML=`
-    <div class="desktop2">
-        <h2 class="countPlayersTitle">Antal spelare: <span id="countPlayers">x</span></h2>
-        <div>
-            <h3 class="gameRulesTitle">Spelets regler:</h3>
-            <p class="gameRulesText">
-                När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
-                I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
-                Tänk även på att rätt färg ska vara i rätt ruta.
-                <br>Lycka till!                 
-            </p>
-        </div>
-        <button id="startGameBtn">STARTA SPELET</button>
-    </div>
-    `;
-    //test ();
-    const startGameBtn = document.querySelector('#startGameBtn');
-    startGameBtn.addEventListener('click', createDesktopThree);
+    contentContainer.innerHTML = '';
 
+    function updateContent(data) {
+        const containerDiv = document.createElement('div');
+        
+        const playerHeader = document.createElement('h2');
+        playerHeader.innerHTML = 'Antal spelare ' + data.length;
+
+        const h3 = document.createElement('h3');
+        h3.innerText = 'Spelets regler:';
+
+        const textArticle = document.createElement('article');
+        textArticle.innerText = `När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
+                    I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
+                    Tänk även på att rätt färg ska vara i rätt ruta.`;
+        const p = document.createElement('p');
+        p.innerHTML = 'Lycka till!';
+        
+        const startGameBtn = document.createElement('button');
+        startGameBtn.innerText = 'Starta spelet';
+        
+        contentContainer.appendChild(containerDiv);
+        containerDiv.append(playerHeader, h3, textArticle, p, startGameBtn);
+
+        console.log(data);
+
+        startGameBtn.addEventListener('click', createDesktopThree);
+    }
+
+    function handleUpdateUsers(data) {
+        contentContainer.innerHTML = '';
+
+        socket.on('updateUsers', handleUpdateUsers);
+        updateContent(data);
+    }
+
+    socket.on('updateUsers', handleUpdateUsers);
 }
+
+
+// import createDesktopThree from "./createDesktopThree.js";
+// import { socket } from "./main.js";
+// //export const users = [];
+
+// export default function createDesktopTwo(){
+//     const contentContainer = document.querySelector(".contentContainer");
+//     contentContainer.innerHTML = '';
+
+//     socket.on('updateUsers', (data) => {
+        
+//         const containerDiv = document.createElement('div');
+        
+//         const playerHeader = document.createElement('h2');
+//         playerHeader.innerHTML = 'Antal spelare ' + data.length;
+
+//         const h3 = document.createElement('h3');
+//         h3.innerText = 'Spelets regler:';
+
+//         const textArticle = document.createElement('article');
+//         textArticle.innerText = `När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
+//                     I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
+//                     Tänk även på att rätt färg ska vara i rätt ruta.`;
+//         const p = document.createElement('p');
+//         p.innerHTML = 'Lycka till!';
+        
+//         const startGameBtn = document.createElement('button');
+//         startGameBtn.innerText = 'Starta spelet';
+        
+//         contentContainer.appendChild(containerDiv);
+//         containerDiv.append(playerHeader, h3, textArticle, p, startGameBtn);
+
+
+//         console.log(data);
+//         //alert(data.length);
+
+//         startGameBtn.addEventListener('click', createDesktopThree);
+
+
+//       });
+    
+// //       const contentContainer = document.querySelector(".contentContainer");
+// //       contentContainer.innerHTML=`
+// //       <div class="desktop2">
+// //       <h2 class="countPlayersTitle">Antal spelare: <span id="countPlayers">x</span></h2>
+// //       <div>
+// //           <h3 class="gameRulesTitle">Spelets regler:</h3>
+// //           <p class="gameRulesText">
+// //               När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
+// //               I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
+// //               Tänk även på att rätt färg ska vara i rätt ruta.
+// //               <br>Lycka till!                 
+// //           </p>
+// //       </div>
+// //       <button id="startGameBtn">STARTA SPELET</button>
+// //   </div>
+// //   `;
+    
+    
+
+    
+//     //test ();
+
+//     // const displayNumberOfUsers = document.querySelector('#countPlayers');
+//     // console.log(displayNumberOfUsers);
+//     // displayNumberOfUsers.innerHTML = users;
+//     // const startGameBtn = document.querySelector('#startGameBtn');
+//     // startGameBtn.addEventListener('click', createDesktopThree);
+
+//     // getPlayers();
+// }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-import createDesktop3 from "./createDesktopThree.js";
-import { renderGameBoard } from "./grid.js";
-import regPlayer from "./color.js";
 import createDesktopTwo from "./createDesktopTwo.js";
-// import test from "./js/setUser.js";
+//import updateUsers from "./getPlayer.js";
 
-//import io from '../node_modules/socket.io-client/dist/socket.io.esm.min.js';
 
-const userName = document.getElementById('inputName')
+const userName = document.getElementById('inputName');
 
 export const socket = io('http://localhost:3000');
 
@@ -17,9 +13,10 @@ socket.on('connect', () => {
 
 socket.emit('userEnter', 1, "hej");
 
-socket.on('getUser', (data) => {
-    console.log(data);
-})
+// socket.on('getUser', (data) => {
+//     console.log(data);
+// });
+
 
   
 
@@ -30,7 +27,9 @@ export const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", ()  => {
     socket.emit('getUser', userName.value);
+        //updateUsers ();
     createDesktopTwo();
+  
 });
 
 
