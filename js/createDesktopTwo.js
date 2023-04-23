2026546cd2a8bc5321c0ff7280c437c338e7eeff
MariaLBovin <maria.bovin@gmail.com>
2023-04-23 19:33:33 +0200

    sending data to desktop 3

--- js/createDesktopTwo.js
+++ js/createDesktopTwo.js
@@ -1,46 +1,117 @@
 import createDesktopThree from "./createDesktopThree.js";
 import { socket } from "./main.js";
 
-export default function createDesktopTwo(){
-    const contentContainer = document.querySelector(".contentContainer");
-    contentContainer.innerHTML = '';
 
-    function updateContent(data) {
-        const containerDiv = document.createElement('div');
-        
-        const playerHeader = document.createElement('h2');
-        playerHeader.innerHTML = 'Antal spelare ' + data.length;
+export default function createDesktopTwo() {
+  const contentContainer = document.querySelector(".contentContainer");
+  contentContainer.innerHTML = "";
 
-        const h3 = document.createElement('h3');
-        h3.innerText = 'Spelets regler:';
+  function updateContent(data) {
+    const containerDiv = document.createElement("div");
 
-        const textArticle = document.createElement('article');
-        textArticle.innerText = `När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
+    const playerHeader = document.createElement("h2");
+    playerHeader.innerHTML = "Antal spelare " + data.length;
+
+    const h3 = document.createElement("h3");
+    h3.innerText = "Spelets regler:";
+
+    const textArticle = document.createElement("article");
+    textArticle.innerText = `När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
                     I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
                     Tänk även på att rätt färg ska vara i rätt ruta.`;
-        const p = document.createElement('p');
-        p.innerHTML = 'Lycka till!';
-        
-        const startGameBtn = document.createElement('button');
-        startGameBtn.innerText = 'Starta spelet';
-        
-        contentContainer.appendChild(containerDiv);
-        containerDiv.append(playerHeader, h3, textArticle, p, startGameBtn);
+    const p = document.createElement("p");
+    p.innerHTML = "Lycka till!";
 
-        console.log(data);
+    const startGameBtn = document.createElement("button");
+    startGameBtn.innerText = "Starta spelet";
 
-        startGameBtn.addEventListener('click', createDesktopThree);
-    }
+    contentContainer.appendChild(containerDiv);
+    containerDiv.append(playerHeader, h3, textArticle, p, startGameBtn);
 
-    function handleUpdateUsers(data) {
-        contentContainer.innerHTML = '';
+    startGameBtn.addEventListener("click", () => {
+        createDesktopThree(data);
+    } );
+  }
 
-        socket.on('updateUsers', handleUpdateUsers);
-        updateContent(data);
-    }
+  function handleUpdateUsers(data) {
+    contentContainer.innerHTML = "";
+    updateContent(data);
+  }
 
-    socket.on('updateUsers', handleUpdateUsers);
+  socket.on("updateUsers", handleUpdateUsers);
+
+  socket.on("fullGame", () => {
+    contentContainer.innerText = "Spelet är fullt, försök igen senare";
+    const backButton = document.createElement("button");
+    backButton.innerText = "Tillbaka till startsidan";
+
+    backButton.addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
+
+    contentContainer.appendChild(backButton);
+  });
 }
+
+// export default function createDesktopTwo(){
+    
+
+//     const contentContainer = document.querySelector(".contentContainer");
+//     contentContainer.innerHTML = '';
+
+//     function updateContent(data) {
+//         const containerDiv = document.createElement('div');
+        
+//         const playerHeader = document.createElement('h2');
+//         playerHeader.innerHTML = 'Antal spelare ' + data.length;
+
+//         const h3 = document.createElement('h3');
+//         h3.innerText = 'Spelets regler:';
+
+//         const textArticle = document.createElement('article');
+//         textArticle.innerText = `När spelet börjar blir du tilldelad en färg (grön, gul, blå eller grön). Färgen kommer visas på nästa sida. 
+//                     I början på spelet kommer en bild att visas i 10 sekunder och sedan försvinner bilden. Du och tillsammans med 3 andra spelar har i uppgift att rita av den tidigare visade bilden.
+//                     Tänk även på att rätt färg ska vara i rätt ruta.`;
+//         const p = document.createElement('p');
+//         p.innerHTML = 'Lycka till!';
+        
+//         const startGameBtn = document.createElement('button');
+//         startGameBtn.innerText = 'Starta spelet';
+        
+//         contentContainer.appendChild(containerDiv);
+//         containerDiv.append(playerHeader, h3, textArticle, p, startGameBtn);
+
+//         //console.log(data);
+
+//         startGameBtn.addEventListener('click', createDesktopThree);
+
+//     }
+
+//     function handleUpdateUsers(data) {
+//         contentContainer.innerHTML = '';
+
+//         socket.on('updateUsers', handleUpdateUsers);
+//         updateContent(data);
+//     }
+
+//     socket.on('updateUsers', handleUpdateUsers);
+
+    
+//     socket.on('fullGame', () => {
+//         contentContainer.innerText = 'Spelet är fullt, försök igen senare';
+//         const backButton = document.createElement('button');
+//         backButton.innerText = 'Tillbaka till startsidan';
+
+//         backButton.addEventListener("click", function() {
+//             window.location.href = "index.html";
+//           });
+
+//         contentContainer.appendChild(backButton);
+
+        
+//     });
+    
+// }
 
 
 // import createDesktopThree from "./createDesktopThree.js";
