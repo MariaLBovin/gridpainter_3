cf691acceeeb775a7ca321c5cc8b317f5d66be8b
MariaLBovin <maria.bovin@gmail.com>
2023-04-24 16:57:58 +0200

    removed files not used

--- js/setUser.js
+++
@@ -1,8 +0,0 @@
-// const socket = io('http://localhost:3000');
-
-// export default function test() {
-//     socket.on("connection", () => {
-//           console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-//         });
-// }
-// test ();
