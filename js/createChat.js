71299e8893f75292fbbb4692d9ea2ddb8c3ec3cf
MariaLBovin <maria.bovin@gmail.com>
2023-04-24 12:16:40 +0200

    new commit

--- js/createChat.js
+++ js/createChat.js
@@ -1,32 +1,34 @@
-export default function createChat () {
-    const chatDiv = document.querySelector('.div-chat');
-    chatDiv.innerHTML = `<div class="chat-container">
-    <header class="chat-header">
-      <h1><i class="fas fa-smile"></i> ChatCord</h1>
-      <a href="index.html" class="btn">Leave Room</a>
-    </header>
-    <main class="chat-main">
-      <div class="chat-sidebar">
-        <h3><i class="fas fa-comments"></i> Room Name:</h3>
-        <h2 id="room-name"></h2>
-        <h3><i class="fas fa-users"></i> Users</h3>
-        <ul id="users">
-        </ul>
-      </div>
-      <div class="chat-messages"></div>
-    </main>
-    <div class="chat-form-container">
-      <form id="chat-form">
-        <input
-          id="msg"
-          type="text"
-          placeholder="Enter Message"
-          required
-          autocomplete="off"
-        />
-        <button class="btn">Skicka</button>
-      </form>
-    </div>
-    </div>`;
-    console.log(chatDiv);
+export const socket = io('http://localhost:3000');
+
+export function chatMsg(data) {
+    console.log(data);
+    const chatForm = document.getElementById('chat-form');
+
+    chatForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const msg = e.target.elements.msg.value;
+        
+        socket.emit('chatMessage', msg, data);
+        console.log(data);
+
+        e.target.elements.msg.value = '';
+        e.target.elements.msg.focus();
+
+    });
+}
+
+export function printChatMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">${message.text}</p>`;
+    document.querySelector('.chat-messages').append(div);
+}
+
+export function printUsers(data) {
+    const userList = document.getElementById('users');
+    userList.innerHTML = `
+    ${data.map(user => `<li>${user.userName}</li>`).join('')}
+    `;
 }
