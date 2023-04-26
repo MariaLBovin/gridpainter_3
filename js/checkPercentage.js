3706ad511678aa2919da8d4cf2ba221c70fdc4e1
nanidam <nani.dam@medieinstitutet.se>
2023-04-26 22:14:34 +0200

    check percentage

--- js/checkPercentage.js
+++ js/checkPercentage.js
@@ -1,27 +1,21 @@
-export default function checkPercentage(array1, array2) {
 
+export default function checkPercentage(img1, conclusionFlat) {
 
     // Är längden samma lika?
-    console.log('array1:', array1);
-    console.log('array2:', array2);
-    
-    if (array1.length === array2.length) {
-
-    // Kontrollera hur många procent som är samma lika.
-    let count = 0;
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        count++;
+    if (img1.length === conclusionFlat.length) {
+      // Kontrollera hur många procent som är samma lika.
+      let count = 0;
+      for (let i = 0; i < img1.length; i++) {
+        if (img1[i].style === conclusionFlat[i].style) {
+          count++;
+        }
       }
-    }
-  
-    //Räkna ut och printa hur många procent man fått.
-    return (count / array1.length) * 100;
+    
+      //Räkna ut och printa hur många procent man fått.
+      return (count / img1.length) * 100;
+    } 
 
-  }
-
-  console.log("Olika långa");
-  return 0;
+    return 0;
 }
 
 
