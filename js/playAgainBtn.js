31633e9c034d0fc034ee3e818389f6eda62b7efe
nanidam <nani.dam@medieinstitutet.se>
2023-04-27 20:09:40 +0200

    :bug: fix render playagainbtn to all players

--- js/playAgainBtn.js
+++
@@ -1,6 +0,0 @@
-export default function playAgain () {
-    const footerDiv = document.querySelector(".footer-div");
-    const playAgainBtn = document.createElement("button");
-    playAgainBtn.innerHTML="Spela igen";
-    footerDiv.prepend(playAgainBtn);
-}
